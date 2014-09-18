fef41924f6f8b3f581173072529beb60d4df26d0
Bryan Shelton <bryan@rover.com>
2014-09-18 07:55:11 -0700

    Markdown readme.

---
+++ README.md
@@ -0,0 +1,4 @@
+## Kombu Encrypted Serializer
+
+An encrypted serializer for Kombu using the python cryptography
+library's Fernet encryption.
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
     from distutils.core import setup
 
 
-readme = open('README.rst').read()
+readme = open('README.md').read()
 
 requirements = [
     'kombu>=3.0.16',
